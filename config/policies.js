c57dd116c98674f89d351abac614d9dead15f872
Han BaHwan <beingbook@gmail.com>
2015-07-22 11:43:14 +0900

    Bearer Authentication Testing

--- config/policies.js
+++ config/policies.js
@@ -28,6 +28,8 @@ module.exports.policies = {
 
   '*': ['passport'],
 
+  '/': ['passport', 'bearerAuth'],
+
   /***************************************************************************
   *                                                                          *
   * Here's an example of mapping some policies to run before a controller    *
